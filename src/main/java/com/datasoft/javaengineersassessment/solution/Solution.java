3a82e292eb1de3b42bc22452a9798a85512bbbd3
tahmidullas <tahmidullas@gmail.com>
2019-06-27 03:10:17 +0600

    Second commit

--- src/main/java/com/datasoft/javaengineersassessment/solution/Solution.java
+++ src/main/java/com/datasoft/javaengineersassessment/solution/Solution.java
@@ -1,5 +1,6 @@
 package com.datasoft.javaengineersassessment.solution;
 
+import com.datasoft.javaengineersassessment.utils.IO;
 public class Solution implements Runnable{
 	
 	
@@ -10,8 +11,15 @@ public class Solution implements Runnable{
 	 */
 	@Override
 	public void run() {
+                IO io=new IO();
+                int n=Integer.parseInt(io.readLine());
+                String temp=io.readLine();
+                int nT=Integer.parseInt(""+temp.charAt(0));
+                int nD=Integer.parseInt(""+temp.charAt(2));
+                //String [] temp=io.readLine.split(" ");
 		System.out.println("All set ...");
 		System.out.println("Goodbye :)");
+                System.out.println("number of objects: "+n+"\nnumber of tables: "+nT+"\nnumber of json array: "+nD);
 	
 	}
 }
